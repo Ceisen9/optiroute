7b6743bc25c956bc8fb09ef019c89f72f2f84240
Craig Eisen <Ceisen91@gmail.com>
2016-04-04 13:27:21 -0400

    Initial deployment on rails success

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( optiroute.js )
+Rails.application.config.assets.precompile += %w( destinations.js optiroute.js trips.controller.js trips.factory.js trips.js )
