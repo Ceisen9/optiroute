192f325872cb035a4485247aadedd7d83ec2165f
jblakeman <blakeman.john@gmail.com>
2016-04-04 11:04:42 -0400

    Add ui-router

--- app/assets/javascripts/optiroute.js
+++ app/assets/javascripts/optiroute.js
@@ -1,10 +1,12 @@
 //= require angular
 //= require angular-resource
+//= require angular-ui-router
 
 "use strict";
 
 (function() {
   angular.module("optiroute", [
+    "ui.router",
     "ngResource"
   ]);
 })();
