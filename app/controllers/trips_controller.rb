ef8d428980a81be8686548696809d5e05d72c452
jblakeman <blakeman.john@gmail.com>
2016-04-04 12:09:04 -0400

    Render JSON, add to create/update methods

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,19 +1,44 @@
 class TripsController < ApplicationController
+  before_action do
+    if params[:id]
+      @trip = Trip.find(params[:id])
+    end
+  end
 
   def index
     @trips = Trip.all
+    respond_to do |format|
+      format.html
+      format.json{ render json: @trips, status: :ok }
+    end
   end
 
   def show
-    @trip = Trip.find(params[:id])
+    render json: @trip, status: :ok
   end
 
-  def new
-    @trip = Trip.new
+  def create
+    @trip = Trip.create!(trip_params)
+    self.add_destination
+    render json: @trip, status: :ok
+  end
+
+  def update
+    @trip.update!(trip_params)
+    self.add_destination
+    render json: @trip, status: :ok
   end
 
   def add_destination
+    if params[:destination_id]
+      @destination = Destination.find(params[:destination_id])
+      @trip.itineraries.create!(destination: @destination)
+    end
+  end
 
+  def destroy
+    @trip.destroy
+    render json: {sucess: true}, status: :ok
   end
 
   private
