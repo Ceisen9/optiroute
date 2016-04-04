7af49475409360a1ebce22df9f13baa78b011f7e
Patrick Moran <patrick_moran@mac.com>
2016-04-04 17:37:34 -0400

    moved itinerary logic to trip controller

--- app/assets/javascripts/trips.factory.js
+++ app/assets/javascripts/trips.factory.js
@@ -16,27 +16,6 @@
       });
       TripFactory.all = TripFactory.query();
 
-      //create destinations attribue on each trip and populate with the correct destinations
-
-      //loop through itinerary
-      ItineraryFactory.all.forEach(function(itinerary) {
-        //loop through all trips
-        TripFactory.all.forEach(function(trip) {
-          //create a destinations attribute for each trip
-          trip.destinations = [];
-          //find all instances of the current trip in the itinerary
-          if (trip.id == itinerary.trip_id) {
-            //for that trip find all destinations that have the corresponding destination_id
-            DestFactory.all.forEach(function(destination) {
-              if (destination.id == itinerary.destination_id) {
-                //add the corresponding destinations to the current trip
-                trip.destinations.push(destination);
-              }
-            });
-          }
-        });
-      });
-
       return TripFactory;
     }
 
