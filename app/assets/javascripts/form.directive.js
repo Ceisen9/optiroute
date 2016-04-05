f67f1a788962d5b3aa0502bebef13e037cc19323
Patrick Moran <patrick_moran@mac.com>
2016-04-05 16:32:59 -0400

    trying to get create trip to work

--- app/assets/javascripts/form.directive.js
+++ app/assets/javascripts/form.directive.js
@@ -3,15 +3,16 @@
 
     angular
       .module("trips")
-      .directive("tripFormController", [
+      .directive("tripForm", [
         "TripFactory",
+        "ItineraryFactory",
         "$state",
         tripFormFunc
       ]);
 
-    function tripFormFunc(TripFactory, $state){
+    function tripFormFunc(TripFactory, ItineraryFactory, $state){
       return {
-        templateUrl: "ng-views/trip.form.html",
+        templateUrl: "ng-views/trips.form.html",
         scope: {
           trip: '=',
           destinations: '='
@@ -22,9 +23,21 @@
       function linkFunc(scope){
         // Placeholder until trip<->destination associations setup
         scope.create = function(){
-          scope.trip.save(function(response){
-            $state.go("tripShow", {}, {reload: true});
+          var destinations = scope.trip.selectedDestinations;
+          var newItinerary = new ItineraryFactory();
+          scope.trip.$save().then(function(response) {
+            for (var id in destinations) {
+              if (destinations[id]) {
+                newItinerary.$save({trip_id: response.id, destination_id: id}, function(data) {
+                  console.log(data);
+                });
+
+              }
+            }
           });
+          // TripFactory.save(scope.trip, function(response){
+          //   $state.go("tripShow", {}, {reload: true});
+          // });
         };
       }
     }
