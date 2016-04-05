1cc3721ab841dde38ad44d26899a97af2de97190
Patrick Moran <patrick_moran@mac.com>
2016-04-05 17:58:20 -0400

    making progress on the delete fix

--- app/assets/javascripts/trips.show.controller.js
+++ app/assets/javascripts/trips.show.controller.js
@@ -12,9 +12,12 @@
     function tripsShowCtrlFunction(TripFactory, $state, $stateParams) {
       var tripsShowVM = this;
       TripFactory.all.$promise.then(function(){
+        console.log(TripFactory.get({id: $stateParams.id}));
+        console.log($stateParams.id);
+        console.log(TripFactory.all);
+        console.log(TripFactory.all[$stateParams.id]);
+
         TripFactory.all.forEach(function(trip) {
-          console.log(trip);
-          console.log($stateParams);
           if(trip.id == $stateParams.id) {
             tripsShowVM.trip = trip;
           }
@@ -23,7 +26,8 @@
 
       tripsShowVM.delete = function() {
         tripsShowVM.trip = TripFactory.get({id: $stateParams.id});
-        TripFactory.all.splice(TripFactory.all.indexOf(tripsShowVM.trip), 1);
+
+        TripFactory.all.splice($stateParams.id, 1);
         tripsShowVM.trip.$delete({id: $stateParams.id}).then(function() {
           $state.go("tripsIndex", {}, {reload: true});
         });
