9ad5a5b7e24281159b59e5ff9c01e7d659969173
Craig Eisen <Ceisen91@gmail.com>
2016-04-06 20:22:38 -0400

    updated styles

--- app/assets/javascripts/form.directive.js
+++ app/assets/javascripts/form.directive.js
@@ -30,8 +30,8 @@
         var input = document.getElementById('pac-input');
 
         var types = document.getElementById('type-selector');
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+        // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        // map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
 
         var autocomplete = new google.maps.places.Autocomplete(input);
         autocomplete.bindTo('bounds', map);
