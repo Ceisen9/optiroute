f73e06be48d01452aa1ca04547baaeaae896fe52
jblakeman <blakeman.john@gmail.com>
2016-04-07 10:26:49 -0400

    Remove unnecessary comments, other minor touches

--- app/assets/javascripts/form.directive.js
+++ app/assets/javascripts/form.directive.js
@@ -28,11 +28,7 @@
           zoom: 12
         });
         var input = document.getElementById('pac-input');
-
         var types = document.getElementById('type-selector');
-        // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-        // map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
-
         var autocomplete = new google.maps.places.Autocomplete(input);
         autocomplete.bindTo('bounds', map);
 
@@ -53,7 +49,6 @@
           } else {
             scope.destinations.push({address: place.formatted_address, name: place.name});
             $("#spots").append("<li class='collection-item'>"+ place.name +"</li>")
-            // console.log(scope.destinations);
           }
 
           // If the place has a geometry, present it on the map.
@@ -107,7 +102,7 @@
             scope.destinations.forEach(function(destination){
               DestinationFactory.save(destination, function(data){
                 ItineraryFactory.save({trip_id: response.id, destination_id: data.id}, function() {
-                  count  += 1
+                  count += 1;
                   console.log(count);
                   console.log(scope.destinations.length);
                   if (count === scope.destinations.length) {
@@ -118,17 +113,7 @@
               });
             });
           });
-        }
-
-        //   scope.destinations.forEach(function(destination){
-        //       console.log(destination);
-        //       DestinationFactory.save({name: destination.name, address: destination.address})
-        //                  .then(function(dest_response){
-        //                     ItineraryFactory.save({trip_id: response.id, destination_id: dest_response.id});
-        //                  });
-        //   });
-        // });
-          //   $state.go("tripShow", {}, {reload: true});
-      };
+        };
+      }
     }
 })();
